5df5d6da55870927e13c0444418be4cefca489b2
Vera Kippel <vera.kippel@outlook.com>
2025-02-27 12:03:36 +0100

    Lägger in JS-doc

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,10 @@ let closeBtn = document.getElementById("close-menu")
 openBtn.addEventListener("click", toggleMenu);
 closeBtn.addEventListener("click", toggleMenu);
 
+/**
+ * Öppnar och stänger hamburgermeny
+ */
 function toggleMenu(){
-    console.log("toggle");
     let navMenuEl = document.getElementById("nav-menu");
 
     let style = window.getComputedStyle(navMenuEl);
@@ -27,6 +29,9 @@ let animationBtn = document.getElementById("animation-three");
 if(animationBtn){
     animationBtn.addEventListener("click", startAnimation);
 }
+/**
+ * Startar en animering (hjärtat)
+ */
 function startAnimation(){
     animationBtn.style.animation ="partyText 2s alternate ease-in-out"
 
@@ -34,14 +39,25 @@ function startAnimation(){
         animationBtn.style.animation="";
     }, 2000);
 }
-
+/**
+ * Anropar loadData vid ladding av sida
+ */
 window.onload = () =>{
     loadData();
 }
 
 //Ladda in data från URL
+/**
+ * @type {Array}
+ */
 let data = [];
+/**
+ * @type {Array}
+ */
 let topApplications = [];
+/**
+ * Hämtar data från URL och anropar sorteringsfunktioner
+ */
 async function loadData(){
     try{
         const response = await fetch("https://studenter.miun.se/~mallar/dt211g/");
@@ -56,7 +72,12 @@ async function loadData(){
         document.querySelector("#error").innerHTML = "<p>Ett fel uppstod - prova igen senare!</p>"
     }
 }
-
+/**
+ * Sorterar data efter program och de 6 med mest ansökningar
+ * Anropar utskriftsfunktion
+ * 
+ * @param {Array} data 
+ */
 function sortPrograms(data){
     const programs = data.filter(a => a.type === "Program");
     programs.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
@@ -66,6 +87,10 @@ function sortPrograms(data){
     printFirstChart(topApplications);
 }
 
+/**
+ * Skriver ut data till stapeldiagram
+ * @param {Array} data 
+ */
 function printFirstChart(data){
     const firstChart = document.getElementById("firstChart");
     new Chart(firstChart, {
@@ -88,7 +113,11 @@ function printFirstChart(data){
         }
     });
 }
-
+/**
+ * Sorterar data efter kurser och de 5 med mest ansöknignar
+ * Anropar utskriftsfunktion
+ * @param {Array} data 
+ */
 function sortCourses(data){
     let fiveCourses = [];
     const courses = data.filter(a => a.type === "Kurs");
@@ -98,27 +127,31 @@ function sortCourses(data){
     }
     printSecondChart(courses);
 }
-    function printSecondChart(data){
-        const secondChart = document.getElementById("secondChart");
-        new Chart(secondChart, {
-            type: "pie",
-            data: {
-                labels: [data[0].name, data[1].name, data[2].name, data[3].name, data[4].name],
-                datasets: [{
-                    label: "Antal ansökningar",
-                    data: [data[0].applicantsTotal, data[1].applicantsTotal, data[2].applicantsTotal, data[3].applicantsTotal, data[4].applicantsTotal],
-                    borderWidth: 1
-                }],
-            },
-                options: {
-                    responsive: false,
-                    maintainAspectRatio: false,
-                    plugins: {
-                        title: {
-                            display: true,
-                            text: "Mest sökta kurser HT23 på Mittuniversitetet"
-                        }
+/**
+ * Skriver ut data till pie-diagram
+ * @param {Array} data 
+ */
+function printSecondChart(data){
+    const secondChart = document.getElementById("secondChart");
+    new Chart(secondChart, {
+        type: "pie",
+        data: {
+            labels: [data[0].name, data[1].name, data[2].name, data[3].name, data[4].name],
+            datasets: [{
+                label: "Antal ansökningar",
+                data: [data[0].applicantsTotal, data[1].applicantsTotal, data[2].applicantsTotal, data[3].applicantsTotal, data[4].applicantsTotal],
+                borderWidth: 1
+            }],
+        },
+            options: {
+                responsive: false,
+                maintainAspectRatio: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: "Mest sökta kurser HT23 på Mittuniversitetet"
                     }
-            }
-        });
-    }
+                }
+        }
+    });
+}
